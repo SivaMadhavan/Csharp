809d3f338650f93d78da6c6564dba01e960602e7
SivaMadhavan <sivamadhavan619@gmail.com>
2023-12-29 19:05:43 +0530

    initial commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+
+await client.ConnectAsync(ipEndPoint);
+while (true)
+{
+    Console.WriteLine("Client : ");
+    var message = Console.ReadLine();
+    if (message == "STOP")
+    {
+        break;
+    }
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    _ = await client.SendAsync(messageBytes, SocketFlags.None);
+    Console.WriteLine($"Socket client sent message: \"{message}\"");
+    string? stop = Console.ReadLine();
+
+
+
+}
+
+client.Shutdown(SocketShutdown.Both);
---
+++ Server/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+Console.WriteLine(ipAddress.ToString());
+
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket listener = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+using Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+var handler = await listener.AcceptAsync();
+while (true)
+{
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+    Console.WriteLine($"Socket server received message: \"{response}");
+
+    if (!string.IsNullOrEmpty(response))
+    {
+        Console.WriteLine($"Socket server received message: \"{response}");
+    }
+}
+
+
+
+
